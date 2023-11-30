139218f7eb5d0a2664fd061c16cccf930a7366d4
+ <+>
2023-11-30 17:33:30 +0300

    completed message

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive)	VALUES('Mr. Chocolate', 30, 'Male', 'Dark Brown', 'Sweet',0);
+INSERT INTO bears(name,age,sex,color,temperament,alive)	VALUES('Rowdy',20,'M','black','fierce',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Tabitha', 15, 'F', 'Brown', 'Calm',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Sergeant Brown', 25, 'M', 'Brown', 'Assertive',0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Melissa', 18, 'F', 'White', 'Playful',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Grinch', 22, 'M', 'Gray', 'Mischievous',0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Wendy', 17, 'F', 'Honey', 'Friendly',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES(NULL, 23, 'M', 'Cinnamon', 'Mysterious',0);
+
+
